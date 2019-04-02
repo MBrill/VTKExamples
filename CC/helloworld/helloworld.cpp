77d8fd2f92f41a1780891544f84f60c8e5d094fd
mast0062 <mast0062@stud.hs-kl.de>
2019-04-02 19:00:52 +0200

    HelloWorld angefangen

--- CC/helloworld/helloworld.cpp
+++ CC/helloworld/helloworld.cpp
@@ -1,4 +1,5 @@
 #include <vtkSphereSource.h>
+#include <vtkCubeSource.h>
 #include <vtkPolyData.h>
 #include <vtkSmartPointer.h>
 #include <vtkPolyDataMapper.h>
@@ -14,59 +15,89 @@
  
 int main(int, char *[])
 {
+	//Ursprung
+	vtkSmartPointer<vtkCubeSource> Ursprung =
+		vtkSmartPointer<vtkCubeSource>::New();
+	Ursprung->SetXLength(0.1);
+	Ursprung->SetYLength(0.1);
+	Ursprung->SetZLength(0.1);
+	Ursprung->SetCenter(0.0, 0.0, 0.0);
 
-  vtkSmartPointer<vtkSphereSource> red = 
+	//KugelVorne
+  vtkSmartPointer<vtkSphereSource> KugelVorne = 
     vtkSmartPointer<vtkSphereSource>::New();
-  red->SetRadius(0.5);
-  red->SetCenter(0.0,0.0,0.0);
-  red->SetPhiResolution(64);
-  red->SetThetaResolution(64);
-  //red->Update();
+  KugelVorne->SetRadius(0.5);
+  KugelVorne->SetCenter(2.0,1.0,0.0);
+  KugelVorne->SetPhiResolution(64);
+  KugelVorne->SetThetaResolution(64);
 
-  vtkSmartPointer<vtkSphereSource> yellow = 
-    vtkSmartPointer<vtkSphereSource>::New();
-  yellow->SetRadius(0.5);
-  yellow->SetCenter(10.0,0.0,0.0);
-  yellow->SetPhiResolution(64);
-  yellow->SetThetaResolution(64);
-  //yellow->Update();
+  //KugelHinten
+  vtkSmartPointer<vtkSphereSource> KugelHinten =
+	  vtkSmartPointer<vtkSphereSource>::New();
+  KugelHinten->SetRadius(0.5);
+  KugelHinten->SetCenter(-2.0, 1.0, 0.0);
+  KugelHinten->SetPhiResolution(64);
+  KugelHinten->SetThetaResolution(64);
+
+  //KugelRechts
+  vtkSmartPointer<vtkSphereSource> KugelRechts =
+	  vtkSmartPointer<vtkSphereSource>::New();
+  KugelRechts->SetRadius(0.5);
+  KugelRechts->SetCenter(0.0, 1.0, 2.0);
+  KugelRechts->SetPhiResolution(64);
+  KugelRechts->SetThetaResolution(64);
+
+  //KugelLinks
+  vtkSmartPointer<vtkSphereSource> KugelLinks =
+	  vtkSmartPointer<vtkSphereSource>::New();
+  KugelLinks->SetRadius(0.5);
+  KugelLinks->SetCenter(0.0, 1.0, -2.0);
+  KugelLinks->SetPhiResolution(64);
+  KugelLinks->SetThetaResolution(64);
   
   //Create a mapper and actor
-  //size_t size = 10;
   std::vector<vtkSmartPointer<vtkPolyDataMapper>> mappers;
   mappers.push_back(vtkSmartPointer<vtkPolyDataMapper>::New());
-  mappers[0]->SetInputConnection(red->GetOutputPort());
+  mappers[0]->SetInputConnection(KugelVorne->GetOutputPort());
   mappers.push_back(vtkSmartPointer<vtkPolyDataMapper>::New());
-  mappers[1]->SetInputConnection(yellow->GetOutputPort());
+  mappers[1]->SetInputConnection(KugelHinten->GetOutputPort());
+  mappers.push_back(vtkSmartPointer<vtkPolyDataMapper>::New());
+  mappers[2]->SetInputConnection(KugelRechts->GetOutputPort());
+  mappers.push_back(vtkSmartPointer<vtkPolyDataMapper>::New());
+  mappers[3]->SetInputConnection(KugelLinks->GetOutputPort());
+  mappers.push_back(vtkSmartPointer<vtkPolyDataMapper>::New());
+  mappers[4]->SetInputConnection(Ursprung->GetOutputPort());
 
-  /*vtkSmartPointer<vtkPolyDataMapper> redMapper =
-	  vtkSmartPointer<vtkPolyDataMapper>::New();
-      redMapper->SetInputConnection(red->GetOutputPort());*/
-
-  vtkSmartPointer<vtkActor> redActor =
+  vtkSmartPointer<vtkActor> KugelVorneActor =
 	  vtkSmartPointer<vtkActor>::New();
-      //redActor->SetMapper(redMapper);
-      redActor->SetMapper(mappers[0]);
-	  redActor->GetProperty()->SetColor(1.0, 0.0, 0.0);
-
-  /*vtkSmartPointer<vtkPolyDataMapper> yellowMapper =
-		  vtkSmartPointer<vtkPolyDataMapper>::New();
-  yellowMapper->SetInputConnection(yellow->GetOutputPort());*/
-
-  vtkSmartPointer<vtkActor> yellowActor =
-		  vtkSmartPointer<vtkActor>::New();
-      //yellowActor->SetMapper(yellowMapper);
-	  yellowActor->SetMapper(mappers[1]);
-      yellowActor->GetProperty()->SetColor(1.0, 1.0, 0.0);
+  KugelVorneActor->SetMapper(mappers[0]);
+  KugelVorneActor->GetProperty()->SetColor(1.0, 0.0, 0.0);
+  vtkSmartPointer<vtkActor> KugelHintenActor =
+	  vtkSmartPointer<vtkActor>::New();
+  KugelHintenActor->SetMapper(mappers[1]);
+  KugelHintenActor->GetProperty()->SetColor(1.0, 0.0, 0.0);
+  vtkSmartPointer<vtkActor> KugelRechtsActor =
+	  vtkSmartPointer<vtkActor>::New();
+  KugelRechtsActor->SetMapper(mappers[2]);
+  KugelRechtsActor->GetProperty()->SetColor(0.0, 0.0, 1.0);
+  vtkSmartPointer<vtkActor> KugelLinksActor =
+	  vtkSmartPointer<vtkActor>::New();
+  KugelLinksActor->SetMapper(mappers[3]);
+  KugelLinksActor->GetProperty()->SetColor(0.0, 0.0, 1.0);
+  vtkSmartPointer<vtkActor> UrsprungActor =
+	  vtkSmartPointer<vtkActor>::New();
+  UrsprungActor->SetMapper(mappers[4]);
+  UrsprungActor->GetProperty()->SetColor(2.0, 2.0, 2.0);
 
   //Create a renderer, render window, and interactor
   vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
 
   //Add the actor to the scene
-  //renderer->AddActor(actors[0]);
-  //renderer->AddActor(actors[1]);
-  renderer->AddActor(redActor);
-  renderer->AddActor(yellowActor);
+  renderer->AddActor(KugelVorneActor);
+  renderer->AddActor(KugelHintenActor);
+  renderer->AddActor(KugelRechtsActor);
+  renderer->AddActor(KugelLinksActor);
+  renderer->AddActor(UrsprungActor);
   renderer->SetBackground(1, 1, 1);
   vtkSmartPointer<vtkRenderWindow> renderWindow = 
     vtkSmartPointer<vtkRenderWindow>::New();

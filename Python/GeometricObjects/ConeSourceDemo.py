f0db3efd5537a1267903e813be1b59cbf3af71d5
MBrill <manfred.brill@hs-kl.de>
2020-09-08 16:02:34 +0200

    Cone überarbeitet

--- Python/GeometricObjects/ConeSourceDemo.py
+++ Python/GeometricObjects/ConeSourceDemo.py
@@ -1,33 +1,35 @@
-# Starten in der Anaconda-Shell mit
-#    python cone.py
+#!/usr/bin/env python
+
 import vtk
 
-coneSource = vtk.vtkConeSource()
-# coneSource.SetResolution(60)
-# coneSource.SetCenter(-2,0,0)
-
-# Mapper und Actor erzeugen und verbinden
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(coneSource.GetOutputPort())
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-
-# Hintergrundfarbe des Fensters mischen
-colors = vtk.vtkNamedColors()
-colors.SetColor("BkgColor", [26, 51, 77, 255])
-
-# Renderer, Fenster und Interactor
-renderer = vtk.vtkRenderer()
-renderer.AddActor(actor)
-renderer.SetBackground(colors.GetColor3d("BkgColor"))  # Background color dark red
-
-renderWindow = vtk.vtkRenderWindow()
-renderWindow.SetPosition(100,100)
-renderWindow.SetSize(500,500)
-renderWindow.AddRenderer(renderer)
-
-renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-renderWindowInteractor.SetRenderWindow(renderWindow)
-
-renderWindow.Render()
-renderWindowInteractor.Start()
+def main():
+    coneSource = vtk.vtkConeSource()
+    # coneSource.SetResolution(60)
+    # coneSource.SetCenter(-2,0,0)
+    # Mapper und Actor erzeugen und verbinden
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(coneSource.GetOutputPort())
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    
+    # Hintergrundfarbe des Fensters mischen
+    colors = vtk.vtkNamedColors()
+    colors.SetColor("BkgColor", [26, 51, 77, 255])
+    # Renderer, Fenster und Interactor
+    renderer = vtk.vtkRenderer()
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d("BkgColor"))  
+    # Background color dark red   
+    renderWindow = vtk.vtkRenderWindow()
+    renderWindow.SetPosition(100,100)
+    renderWindow.SetSize(500,500)
+    renderWindow.AddRenderer(renderer)
+    
+    renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+    renderWindowInteractor.SetRenderWindow(renderWindow)
+    
+    renderWindow.Render()
+    renderWindowInteractor.Start()
+    
+if __name__ == '__main__':
+    main()    
--- README.md
+++
@@ -1,6 +0,0 @@
-# VTKExamples
-Beispiele for VTK and ParaView.
-
-Aktuell sind Beispiele für VTK in C++ und Python enthalten.
-
-Im Verzeichnis CMake findet man zwei kleine Beispiele für die Verwendung von cmake und ctest (Unit-Tests mit cmake).
